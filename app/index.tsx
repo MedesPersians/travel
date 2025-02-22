28306f89d38795c67762327bd33e56165a2ff45c
Omamede Sefia <medesefia@gmail.com>
2025-02-22 18:36:38 +0100

    minor changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>This is text.</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to About Page
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#25292e",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    color: "white",
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    textDecorationColor: "#fff",
+    color: "#fff",
+  },
+});
